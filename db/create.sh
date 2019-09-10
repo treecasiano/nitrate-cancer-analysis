c0abf13e17c06f80dda5cb7e1baa9aefcbf21342
Tree Casiano <7842151+treecasiano@users.noreply.github.com>
2019-09-10 08:49:21 -0700

    Remove backend and db files and utilities

--- db/create.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-  # -d postgres:10.7
-
-  docker run \
-  --name nitrate-cancer-analysis \
-  -e POSTGRES_PASSWORD=docker \
-  -e POSTGRES_DB=dev \
-  -p 5433:5432 \
-  -d mdillon/postgis
