7fab1d3353e770974090eb8b69c049c20851d89f
Tree Casiano <tree.pdx@gmail.com>
2019-09-10 08:38:00 -0700

    Get geojson via a flat file instead of API endpoint

--- src/api/example.js
+++ src/api/example.js
@@ -1,32 +1,38 @@
 import axios from "axios";
 
 export default {
-  get(id) {
-    return axios({
-      method: "get",
-      url: `/api/example/${id}`,
-    });
-  },
-  list() {
-    return axios({
-      method: "get",
-      url: `/api/example`,
-    });
-  },
+  // get(id) {
+  //   return axios({
+  //     method: "get",
+  //     url: `/api/example/${id}`,
+  //   });
+  // },
+  // list() {
+  //   return axios({
+  //     method: "get",
+  //     url: `/api/example`,
+  //   });
+  // },
   getGeoJSON() {
     return axios({
       method: "get",
-      url: `/api/exampleGeoJSON`,
+      url: `exampleGeoJSON.geojson`,
     });
   },
-  put(example) {
-    const url = `/api/example/${example.id}`;
-    const data = Object.assign({}, example);
+  // getGeoJSON() {
+  //   return axios({
+  //     method: "get",
+  //     url: `/api/exampleGeoJSON`,
+  //   });
+  // },
+  // put(example) {
+  //   const url = `/api/example/${example.id}`;
+  //   const data = Object.assign({}, example);
 
-    return axios({
-      data,
-      method: "put",
-      url,
-    });
-  },
+  //   return axios({
+  //     data,
+  //     method: "put",
+  //     url,
+  //   });
+  // },
 };
