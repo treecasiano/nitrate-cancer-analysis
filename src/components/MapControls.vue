415e59d667112ef8774c79452011c88be75a2867
Tree Casiano <7842151+treecasiano@users.noreply.github.com>
2019-09-13 00:08:27 -0700

    Add a navigation drawer

--- src/components/MapControls.vue
+++ src/components/MapControls.vue
@@ -1,16 +1,39 @@
 <template>
-  <v-container>
-    <!-- <v-layout text-left wrap> -->
-    <!-- <v-flex>
-        <h2 class="font-weight-bold">Map Controls</h2>
-    </v-flex>-->
-    <!-- </v-layout> -->
-  </v-container>
+  <v-card>
+    <v-navigation-drawer v-model="drawer" :mini-variant.sync="mini" permanent>
+      <template v-slot:prepend>
+        <v-list-item v-if="mini">
+          <v-btn icon @click.stop="mini = !mini">
+            <v-icon>mdi-chevron-right</v-icon>
+          </v-btn>
+        </v-list-item>
+        <v-list-item v-if="!mini">
+          <v-list-item-title>Map Controls</v-list-item-title>
+
+          <v-btn icon @click.stop="mini = !mini">
+            <v-icon>mdi-chevron-left</v-icon>
+          </v-btn>
+        </v-list-item>
+
+        <v-divider></v-divider>
+      </template>
+    </v-navigation-drawer>
+  </v-card>
 </template>
 
 <script>
 export default {
-  data: () => ({}),
+  data() {
+    return {
+      drawer: true,
+      items: [
+        { title: "Home", icon: "mdi-home-city" },
+        { title: "My Account", icon: "mdi-account" },
+        { title: "Users", icon: "mdi-account-group-outline" },
+      ],
+      mini: false,
+    };
+  },
 };
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <MapComponent height="100" loading="loading" offsetHeight="90" width="100" />
+    <MapComponent height="100" loading="loading" offsetHeight="50" width="100" />
   </div>
 </template>
 
