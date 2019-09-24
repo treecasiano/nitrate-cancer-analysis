a6ce2e67984a01f6e8ecdf21ffee176aaf5b5f1a
Tree Casiano <7842151+treecasiano@users.noreply.github.com>
2019-09-24 07:55:32 -0700

    Implement spinner

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,12 +2,14 @@ import Vue from "vue";
 import Vuetify from "vuetify/lib";
 import "vuetify/dist/vuetify.min.css";
 import colors from "vuetify/lib/util/colors";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
+import "@fortawesome/fontawesome-free/css/all.css";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
   icons: {
-    iconfont: "mdi",
+    iconfont: "fa",
   },
   theme: {
     themes: {
